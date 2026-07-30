b4b18f90b937219122f94bc554ca7b2308af0c32
Uday sankar <udaycollagebwu@gmail.com>
2026-07-30 16:24:42 +0530

    change the code

--- day4/largest_three.py
+++ day4/largest_three.py
@@ -2,11 +2,13 @@ number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
 number3 = int(input("Enter the third number: "))
 
-if number1 >= number2 and number1 >= number3:
-    print("The number", number1, "is the biggest among the three!")
-elif number2 >= number1 and number2 >= number3:
-    print("The number", number2, "is the biggest among the three!")
-elif number3 >= number1 and number3 >= number2:
-    print("The number", number3, "is the biggest among the three!")
+if number1 >= number2:
+    if number1>number3:
+        print("The number ",number1, " is bigest .")
+    else:
+        print("The number ",number3, " is the bigest .")
 else:
-    print("All are the same numbers!")
+    if number2>number3:
+        print("The number ",number2," is the bigest ")
+    else:
+        print("The number ",number3," is the bigest ")
