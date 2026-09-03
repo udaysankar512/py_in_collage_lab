2aa827119a0afcf8056a4231ee97c63eecded1cc
Uday sankar <udaycollagebwu@gmail.com>
2026-09-03 14:25:19 +0530

    change the logice

--- day8/small_three.py
+++ day8/small_three.py
@@ -3,17 +3,19 @@
 def smallest(nu1,nu2,nu3): 
     if nu1 < nu2:
       if nu1 < nu3:
-        print("The smallest number is :",nu1)
+        return nu1
       else:
-        print("The smallest number is :",nu3)
+        return nu3
     else:
         if nu2 < nu3:
-            print("The smallest number is :",nu2)
+            return nu2
         else:
-            print("The smallest number is :",nu3)
+            return nu3
 
 num1 = int(input("Enter the first number :"))
 num2 = int(input("Enter the second number :"))
 num3 = int(input("Enter the third number :"))
 
-smallest(num1,num2,num3)
+result = smallest(num1,num2,num3)
+
+print("The smallest number is :",result)
