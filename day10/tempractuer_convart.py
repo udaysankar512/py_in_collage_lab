d86f2cf1c8d254e9aa5852c10d6afec12bcf08ab
Uday sankar <udaycollagebwu@gmail.com>
2026-09-10 16:04:57 +0530

    add a comment in the line

--- day10/tempractuer_convart.py
+++ day10/tempractuer_convart.py
@@ -2,7 +2,8 @@
 # degrees to their equivalent Celsius degrees using list comprehension.
 
 fahrenheit = [32, 68, 86, 104, 122]
-celsius = [(temp - 32) * 5 / 9 for temp in fahrenheit]  #Formula : (0°C × 9/5) + 32 = 32°F
+celsius = [(temp - 32) * 5 / 9 for temp in fahrenheit]  #Formula : (0°C × 9/5) + 32 = 32°F  
+                                                        # (Fahrenheit - 32) × 5 ÷ 9 = Celsius
 
 
 print("Fahrenheit:", fahrenheit)
